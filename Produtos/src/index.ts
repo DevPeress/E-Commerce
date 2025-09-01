50883a7ff74ae85c6ddeaae61fa7f6eeb14d6789
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 19:52:26 -0300

    Adição do JWT nos produtos

--- Produtos/src/index.ts
+++ Produtos/src/index.ts
@@ -1,7 +1,8 @@
 import app from "./lib/express";
+import { authMiddleware } from "./middlewares/auth";
 import produtosRouter from './routes/produtos'
 
-app.use("/produtos", produtosRouter)
+app.use("/produtos", authMiddleware, produtosRouter)
 
 app.listen(3005, () => {
   console.log(`🚀 Microserviço de Cargos rodando na porta 3005`);
