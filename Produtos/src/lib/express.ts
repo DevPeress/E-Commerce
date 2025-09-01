d2d0c5be6e1a0be6aad3a092a8ddff4dd8bcd0cb
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 14:02:50 -0300

    Adição do rate-limit

--- Produtos/src/lib/express.ts
+++ Produtos/src/lib/express.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 const app = express();
 
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  message: 'Muitas tentativas de pesquisa de produtos. Tente novamente em 15 minutos.'
+});
+
+app.use(limiter);
 
 export default app;
