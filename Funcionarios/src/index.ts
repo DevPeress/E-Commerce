8751833b265383a5450067d6897926c0ab8a9a9e
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 19:46:30 -0300

    Adição do JWT nos funcionarios

--- Funcionarios/src/index.ts
+++ Funcionarios/src/index.ts
@@ -1,7 +1,8 @@
 import app from "./lib/express";
+import { authMiddleware } from "./middlewares/auth";
 import funcionariosRouter from "./routes/funcionarios";
 
-app.use("/funcionarios", funcionariosRouter)
+app.use("/funcionarios", authMiddleware, funcionariosRouter)
 
 app.listen(3001, () => {
   console.log(`🚀 Microserviço de Funcionários rodando na porta 3001`);
