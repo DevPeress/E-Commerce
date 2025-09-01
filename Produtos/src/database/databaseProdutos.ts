176fd9e2129ec1c2a852f70ce05a5cfce4efe609
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 13:26:16 -0300

    Criação do databaseProdutos para separação do código da rota e do banco de dados

---
+++ Produtos/src/database/databaseProdutos.ts
@@ -0,0 +1,100 @@
+import db from "../lib/mysql";
+import { InsertInput } from "../schemas/insertSchemas";
+import { UpdateInput } from "../schemas/updateSchemas";
+import { Produtos } from "../types/produtos";
+
+export const produtosDB = {
+    async getAll(): Promise<{ sucess: boolean, data?: Produtos[], error?: string }> {
+        try {
+            const [rows] = await db.query<Produtos[]>('SELECT * FROM Produtos')
+            if (rows.length === 0) return { sucess: false, error: "Não possui produtos na empresa!" }
+            return { sucess: true, data: rows }
+        } catch(err) {
+            console.error("Produtos GetAll: ", err)
+            return { sucess: false, error: "Erro ao encontrar os produtos!" }
+        }
+    },
+
+    async getById(id: number): Promise<{ sucess: boolean, data?: Produtos[], error?: string }> {
+        try {
+            const [rows] = await db.query<Produtos[]>('SELECT * FROM Produtos WHERE id = ?', [id])
+            if (rows.length === 0) return { sucess: true, error: "Não foi localizado produto com esse ID!" }
+            return { sucess: true, data: rows }
+        } catch(err) {
+            console.error("Produtos GetById: ", err)
+            return { sucess: false, error: "Erro ao localizar produto pelo ID!" }
+        }
+    },
+
+    async getByName(nome: string): Promise<{ sucess: boolean, data?: Produtos[], error?: string }> {
+        try {
+            const [rows] = await db.query<Produtos[]>('SELECT * FROM Produtos WHERE nome = ?', [nome])
+            if (rows.length === 0) return { sucess: false, error: "Não foi localizado produto com esse nome!" }
+            return { sucess: true, data: rows }
+        } catch(err) {
+            console.error("Produtos GetByName: ", err)
+            return { sucess: false, error: "Erro ao localizar produto pelo nome!!" }
+        }
+    },
+
+    async postProduto(data: InsertInput): Promise<{ sucess: boolean, data?: Produtos[], error?: string }> {
+        try {
+            const [rows] = await db.execute<Produtos[]>('INSERT INTO Produtos(nome,quantidade,descricao) VALUES(?,?,?)', [data.nome, data.quantidade, data.descricao])
+            return { sucess: true, data: rows }
+        } catch(err) {
+            console.error("Produtos PostProduto: ", err)
+            return { sucess: false, error: "Erro ao criar o produto!" }
+        }
+    },
+
+    async putProduto(data: UpdateInput): Promise<{ sucess: boolean, error?: string }> {
+        try {
+            const produto = await produtosDB.getById(data.id)
+            if (!produto.sucess) return { sucess: false, error: produto.error }
+            
+            await db.execute(`UPDATE Produtos SET ${data.tipo} = ? WHERE id = ?`, [data.valor, data.id])
+            return { sucess: true }
+        } catch(err) {
+            console.error("Produtos PutProduto: ", err)
+            return { sucess: false, error: "Erro ao criar o produto!" }
+        }
+    },
+
+    async putAll(ajustes: Produtos[]): Promise<{ sucess: boolean, error?: string }> {
+        const connection = await db.getConnection()
+        try {
+            await connection.beginTransaction()
+
+            await Promise.all(ajustes.map((infos) => connection.execute("UPDATE Produtos SET nome = ?, quantidade = ?, descricao = ? WHERE id = ?", [infos.nome, infos.quantidade, infos.descricao, infos.id])))
+
+            await connection.commit()
+            return { sucess: true }
+        } catch(err) {
+            await connection.rollback()
+            console.error("Produtos PutAll: ", err)
+            return { sucess: false, error: "Erro ao atualizar todos os produtos!!" }
+        }finally {
+            connection.release()
+        }
+    },
+
+    async deleteById(id: number): Promise<{ sucess: boolean, error?: string }> {
+        try {
+            await db.execute('DELETE FROM Produtos WHERE id = ?', [id])
+            return { sucess: true }
+        } catch(err){
+            console.error("Produtos PutAll: ", err)
+            return { sucess: false, error: "Erro ao deletar o produto!!" }
+        }
+    },
+
+    async deleteAll(): Promise<{ sucess: boolean, error?: string }> {
+        try {
+            await db.execute('DELETE FROM Produtos')
+            return { sucess: true }
+        } catch(err){
+            console.error("Produtos PutAll: ", err)
+            return { sucess: false, error: "Erro ao deletar todos os produtos!!" }
+        }
+    }
+}
--- Produtos/src/routes/produtos.ts
+++ Produtos/src/routes/produtos.ts
@@ -5,121 +5,78 @@ import type { Produtos } from '../types/produtos';
 import { validate } from "../middlewares/validate";
 import { InsertInput, insertSchema } from "../schemas/insertSchemas";
 import { UpdateInput, updateSchema } from "../schemas/updateSchemas";
+import { produtosDB } from "../database/databaseProdutos";
 
 router.get("/", async (req: Request, res: Response) => {
-    try {
-        const [rows] = await db.query<Produtos[]>('SELECT * FROM Produtos')
+    const dados = await produtosDB.getAll()
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
 
-        if (rows.length === 0) return res.status(500).json({ error: "Não foi possivel localizar a lista de produtos!" })
-        
-        return res.json(rows)
-    } catch(err) {
-        console.error("MicroServiço Produtos GET: ", err)
-        return res.status(500).json({ error: "Não foi possivel localizar a lista de produtos!" })
-    }
+    return res.json(dados.data)
 })
 
 router.get("/:id", async (req: Request, res: Response) => {
     const id: number = parseInt(req.params.id)
     if (!id) return res.status(400).json({ error: "ID não informado do produto!" })
+    
+    const dados = await produtosDB.getById(id)
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
 
-    try {
-        const [rows] = await db.query<Produtos[]>('SELECT * FROM Produtos WHERE id = ?', [id])
-
-        if (rows.length === 0) return res.status(500).json({ error: "Não foi possivel localizar a lista de produtos!" })
-        
-        return res.json(rows)
-    } catch(err) {
-        console.error("MicroServiço Produtos GET/:id: ", err)
-        return res.status(500).json({ error: "Não foi possivel localizar a lista de produtos!" })
-    }
+    return res.status(200).json(dados.data)
 })
 
 router.get("/:nome", async (req: Request, res: Response) => {
     const nome: string = req.params.nome
     if (!nome) return res.status(400).json({ error: "Nome não informado do produto!" })
 
-    try {
-        const [rows] = await db.query<Produtos[]>('SELECT * FROM Produtos WHERE nome = ?', [nome])
+    const dados = await produtosDB.getByName(nome)
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
 
-        if (rows.length === 0) return res.status(500).json({ error: "Não foi possivel localizar a lista de produtos!" })
-        
-        return res.json(rows)
-    } catch(err) {
-        console.error("MicroServiço Produtos GET/:nome: ", err)
-        return res.status(500).json({ error: "Não foi possivel localizar a lista de produtos!" })
-    }
+    return res.status(200).json(dados.data)
 })
 
 router.post("/", validate(insertSchema), async (req: Request, res: Response) => {
     const data = req.body as InsertInput
 
-    try {
-        const [rows] = await db.execute('INSERT INTO Produtos(nome,quantidade,descricao) VALUES(?,?,?)', [data.nome, data.quantidade, data.descricao])
-        return res.json(rows)
-    } catch(err) {
-        console.error("MicroServiço Produtos POST: ", err)
-        return res.status(500).json({ error: "Não foi possível criar o produto!" })
-    }
+    const dados = await produtosDB.postProduto(data)
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
+
+    return res.status(200).json(dados.data)
 })
 
 router.put("/", validate(updateSchema),async (req: Request, res: Response) => {
     const data = req.body as UpdateInput
 
-    try {
-        const [rows] = await db.query<Produtos[]>('SELECT id FROM Produtos WHERE id = ?', [data.id])
-        if (rows.length === 0) return res.status(400).json({ error: "Produto inexistente!" })
+    const dados = await produtosDB.putProduto(data)
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
 
-        await db.execute(`UPDATE Produtos SET ${data.tipo} = ? WHERE id = ?`, [data.valor, data.id])
-        return res.status(200).json({ message: "Produto atualizado com sucesso!" })
-    } catch(err) {
-        console.error("MicroServiço Produtos PUT: ", err)
-        return res.status(500).json({ error: "erro ao atualizar o produto!" })
-    }
+    return res.status(200).json({ message: "Produto foi atualizado com sucesso!" })
 })
 
 router.put("/all", async (req: Request, res: Response) => {
-  const { ajustes } = req.body as { ajustes: Produtos[] }
-  if (!ajustes || !Array.isArray(ajustes)) return res.status(400).json({ error: "Ajuste inviados de forma inválida!" })
+    const { ajustes } = req.body as { ajustes: Produtos[] }
+    if (!ajustes || !Array.isArray(ajustes)) return res.status(400).json({ error: "Ajuste inviados de forma inválida!" })
 
-  const connection = await db.getConnection()
-  try {
-    await connection.beginTransaction()
+    const dados = await produtosDB.putAll(ajustes)
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
 
-    await Promise.all(ajustes.map((infos) => connection.execute("UPDATE Produtos SET nome = ?, quantidade = ?, descricao = ? WHERE id = ?", [infos.nome, infos.quantidade, infos.descricao, infos.id])))
-
-    await connection.commit()
-    return res.json({ success: true, message: "Produtos atualizados com sucesso!" })
-  } catch(err) {
-    await connection.rollback()
-    console.error("MicroServiço Produtos PUT/ALL: ", err)
-    return res.status(500).json({ error: "Erro ao atualizar os Produtos!" })
-  } finally {
-    connection.release()
-  }
+    return res.status(200).json({ message: "Produtos foram atualizados com sucesso!" })
 })
 
 router.delete("/", async (req: Request, res: Response) => {
     const { id } = req.body as { id: number }
     if (!id) return res.status(400).json({ error: "ID não informado do produto!" })
 
-    try {
-        await db.execute('DELETE FROM Produtos WHERE id = ?', [id])
-        return res.status(200).json({ message: "Sucesso ao deletar o produto!" })
-    } catch(err) {
-        console.error("MicroServiço DELETE: ", err)
-        return res.status(500).json({ error: "Erro ao deletar o produto!" })
-    }
+    const dados = await produtosDB.deleteById(id)
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
+
+    return res.status(200).json({ message: "Produtos foi deletado com sucesso!" })
 })
 
 router.delete("/all", async (req: Request, res: Response) => {
-    try {
-        await db.execute('DELETE FROM Produtos')
-        return res.status(200).json({ message: "Sucesso ao deletar todos os produtos!" })
-    } catch(err) {
-        console.error("MicroServiço DELETE/ALL: ", err)
-        return res.status(500).json({ error: "Erro ao deletar o produto!" })
-    }
+    const dados = await produtosDB.deleteAll()
+    if (!dados.sucess) return res.status(404).json({ error: dados.error })
+
+    return res.status(200).json({ message: "Produtos foram deletados com sucesso!" })
 })
 
 export default router
