803b896e7527641a25d331bf5439270256f299ac
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 19:41:15 -0300

    Adição do JWT no login

--- Login/src/database/databaseLogin.ts
+++ Login/src/database/databaseLogin.ts
@@ -4,13 +4,13 @@ import { LoginInput } from "../schemas/loginSchemas"
 import { Login } from "../types/login"
 
 export const LoginDB = {
-    async getLogin(dados: LoginInput): Promise<{ sucess: boolean, error?: string }> {
+    async getLogin(dados: LoginInput): Promise<{ sucess: boolean, data?: Login[], error?: string }> {
         const [rows] = await db.query<Login[]>('SELECT email, senha FROM Clientes WHERE email = ? LIMIT 1', [dados.email])
         if (rows.length === 0) return { sucess: false, error: "Email ou senha estão incorretos!" }
 
         const senhaCorreta: boolean = await VerificarSenha(dados.senha,rows[0].senha)
         if (!senhaCorreta) return { sucess: false, error: "Email ou senha estão incorretos!" }
         
-        return { sucess: true }
+        return { sucess: true, data: rows }
     }
 }
--- Login/src/routes/login.ts
+++ Login/src/routes/login.ts
@@ -3,14 +3,18 @@ import router from "../lib/router";
 import { validate } from "../middlewares/validate";
 import { LoginInput, loginSchema } from "../schemas/loginSchemas";
 import { LoginDB } from "../database/databaseLogin";
+import { generateToken } from "../lib/jwt";
 
 router.post("/", validate(loginSchema), async (req: Request, res: Response) => {
     const data = req.body as LoginInput
 
     const dados = await LoginDB.getLogin(data)
     if (!dados.sucess) return res.status(404).json({ error: dados.error })
+    if (!dados.data) return res.status(404).json({ error: "Erro ao realizar o login" })
 
-    return res.status(200).json({ message: "Login efetuado!" })
+    const token = generateToken({ id: dados.data[0].id, email: dados.data[0].email });
+
+    return res.json({ token })
 })
 
 export default router
