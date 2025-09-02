fe036edbe89b6f785df726ac7c97b0cefeded11e
Peres <fabricioperesdsantos@gmail.com>
2025-09-02 18:53:59 -0300

    Adição do cors

--- services/Cargos/src/lib/express.ts
+++ services/Cargos/src/lib/express.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
+import cors from 'cors'
+import { PORT } from "./config";
+
 const app = express();
+
+const options: cors.CorsOptions = {
+  origin: 'http://localhost:' + PORT
+}
 
 app.use(express.json());
 
