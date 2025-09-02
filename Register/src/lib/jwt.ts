d5e760c772f0af21f25ee25e41633e96e2d4fc0d
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 21:28:42 -0300

    Mudando env do jwt

--- Register/src/lib/jwt.ts
+++ Register/src/lib/jwt.ts
@@ -1,14 +1,8 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
+import { JWT_SECRET } from "./config";
 
-dotenv.config();
-
-const JWT_SECRET = process.env.JWT_ENV
 const JWT_EXPIRES_IN = "8hr"
 
 export function generateToken(payload: object) {
-  if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET não definido");
-  }
   return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 }
