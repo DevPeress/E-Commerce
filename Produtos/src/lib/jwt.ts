af009c66d35ae87a24758b077165b1453d0d67bd
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 21:29:04 -0300

    Mudando env do jwt

--- Produtos/src/lib/jwt.ts
+++ Produtos/src/lib/jwt.ts
@@ -1,12 +1,6 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
-const JWT_SECRET = process.env.JWT_ENV
+import { JWT_SECRET } from "./config";
 
 export function verifyToken(token: string) {
-  if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET não definido");
-  }
   return jwt.verify(token, JWT_SECRET);
 }
