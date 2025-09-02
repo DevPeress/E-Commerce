e02811bdfdc7f5cc3b689b0a1fe50bb55f43e2c5
Peres <fabricioperesdsantos@gmail.com>
2025-09-02 18:52:54 -0300

    Adição do cors

--- services/Authentication/src/lib/express.ts
+++ services/Authentication/src/lib/express.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
+import cors from 'cors';
+import { PORT } from "./config";
+
 const app = express();
 
+const options: cors.CorsOptions = {
+  origin: 'http://localhost:'+PORT
+};
+
+app.use(cors(options));
 app.use(express.json());
 
 const limiter = rateLimit({
