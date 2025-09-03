07dc97f0cab12c846665da2025a7bf2c26c5726a
Peres <fabricioperesdsantos@gmail.com>
2025-09-02 21:51:02 -0300

    Adição do logger.error no validate

--- services/Funcionarios/src/middlewares/validate.ts
+++ services/Funcionarios/src/middlewares/validate.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ZodSchema } from "zod";
 import { Request, Response, NextFunction } from "express";
+import logger from "../lib/pino";
 
 export function validate(schema: ZodSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -8,6 +9,7 @@ export function validate(schema: ZodSchema) {
       schema.parse(req.body);
       next();
     } catch (error: any) {
+      logger.error("Validate: " + error);
       return res.status(400).json({
         error: "Dados inválidos",
         details: error.errors,
