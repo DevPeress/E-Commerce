c2657afa9fec8e71fe88a94830fa756c4e0ce6d4
Peres <fabricioperesdsantos@gmail.com>
2025-09-04 14:00:51 -0300

    Verificação do cargo para acessar o serviço Cargos

--- services/Cargos/src/middleware/auth.ts
+++ services/Cargos/src/middleware/auth.ts
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import logger from "../lib/pino";
 import { JWT_SECRET } from "../lib/config";
+import { MyJwtPayload } from "../types/cargos";
 
 export function authMiddleware(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
@@ -14,8 +14,13 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    (req as any).user = decoded; // injeta usuário no request
+    const decoded = jwt.verify(token, JWT_SECRET) as MyJwtPayload;
+    req.user = decoded;
+
+    if (decoded.cargo !== "Admin") {
+      return res.status(403).json({ message: "Acesso negado: apenas Admin" });
+    }
+
     next();
   } catch (error) {
     logger.error("Auth: " + error);
--- services/Cargos/src/types/cargos.d.ts
+++ services/Cargos/src/types/cargos.d.ts
@@ -10,3 +10,15 @@ export interface Criar {
   id: number;
   perms: [];
 }
+
+interface MyJwtPayload extends JwtPayload {
+  id: number;
+  email: string;
+  cargo: string;
+}
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: MyJwtPayload;
+  }
+}
