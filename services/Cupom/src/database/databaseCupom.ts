5812ca837dc60a8fa7b28d01fc44c5e289c85956
Peres <fabricioperesdsantos@gmail.com>
2025-09-04 12:27:37 -0300

    Adição do Get By ID

--- services/Cupom/src/database/databaseCupom.ts
+++ services/Cupom/src/database/databaseCupom.ts
@@ -10,7 +10,19 @@ export const CupomDB = {
     } catch (err) {
       logger.error("Cupom GetAll: " + err);
       console.error("Cupom GetAll: ", err);
-      return { sucess: false, error: "Erroa o localizar os cupons!" };
+      return { sucess: false, error: "Erro ao localizar os cupons!" };
+    }
+  },
+
+  async getById(id: number): Promise<{ sucess: boolean; data?: []; error?: string }> {
+    try {
+      const [rows] = await db.query<[]>("SELECT * FROM Cupom WHERE id = ?", [id]);
+      if (rows.length === 0) return { sucess: false, error: "Não possui cupom esse ID!" };
+      return { sucess: true, data: rows };
+    } catch (err) {
+      logger.error("Cupom GetById: " + err);
+      console.error("Cupom GetById: ", err);
+      return { sucess: false, error: "Erro ao localizar o cupom!" };
     }
   },
 };
--- services/Cupom/src/routes/cupom.ts
+++ services/Cupom/src/routes/cupom.ts
@@ -7,3 +7,12 @@ router.get("/", async (req: Request, res: Response) => {
   if (!dados.sucess) return res.status(404).json({ error: dados.error });
   return res.json(dados.data);
 });
+
+router.get("/id/:id", async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id);
+  if (!id) return res.status(400).json({ error: "ID Não informado" });
+
+  const dados = await CupomDB.getById(id);
+  if (!dados.sucess) return res.status(404).json({ error: dados.error });
+  return res.json(dados.data);
+});
