704afc8a33ecbaceb24441637d07247a3c8349b9
Peres <fabricioperesdsantos@gmail.com>
2025-09-04 23:45:14 -0300

    Adição do loggers em mais opções

--- services/Cupom/src/database/databaseCupom.ts
+++ services/Cupom/src/database/databaseCupom.ts
@@ -6,6 +6,7 @@ import { Cupom } from "../types/cupom";
 export const CupomDB = {
   async getAll(): Promise<{ sucess: boolean; data?: Cupom[]; error?: string }> {
     try {
+      logger.info("Requisitou todos os cupons!");
       const [rows] = await db.query<Cupom[]>("SELECT * FROM Cupom");
       if (rows.length === 0) return { sucess: false, error: "Não possui cupons registrados!" };
       return { sucess: true, data: rows };
@@ -18,6 +19,7 @@ export const CupomDB = {
 
   async getById(id: number): Promise<{ sucess: boolean; data?: Cupom[]; error?: string }> {
     try {
+      logger.info("Requisitou cupom de ID: " + id);
       const [rows] = await db.query<Cupom[]>("SELECT * FROM Cupom WHERE id = ?", [id]);
       if (rows.length === 0) return { sucess: false, error: "Não possui cupom esse ID!" };
       return { sucess: true, data: rows };
@@ -30,6 +32,7 @@ export const CupomDB = {
 
   async getByName(name: string): Promise<{ sucess: boolean; data?: Cupom[]; error?: string }> {
     try {
+      logger.info("Requisitou cupom de Nome: " + name);
       const [rows] = await db.query<Cupom[]>("SELECT * FROM Cupom WHERE id = ?", [name]);
       if (rows.length === 0) return { sucess: false, error: "Não possui cupom esse Nome!" };
       return { sucess: true, data: rows };
@@ -43,8 +46,13 @@ export const CupomDB = {
   async postCupom(data: CupomInput): Promise<{ sucess: boolean; error?: string }> {
     try {
       const dados = await CupomDB.getByName(data.nome);
-      if (dados.sucess) return { sucess: false, error: "Cupom com o mesmo nome criado já!" };
+      if (dados.sucess) {
+        logger.info("Tentou criar com o mesmo nome definido: " + data.nome);
+        return { sucess: false, error: "Cupom com o mesmo nome criado já!" };
+      }
 
+      const tipo = data.tipo ? "fixo" : "percentual";
+      logger.info("Criou o cupom: " + data.nome + ", valor: " + data.valor + " tipo: " + tipo);
       await db.execute("INSERT INTO Cupom(nome,valor,tipo) VALUES(?,?,?)", [
         data.nome,
         data.valor,
@@ -61,8 +69,12 @@ export const CupomDB = {
   async putCupom(data: CupomInput): Promise<{ sucess: boolean; error?: string }> {
     try {
       const dados = await CupomDB.getByName(data.nome);
-      if (!dados.sucess) return { sucess: false, error: dados.error };
+      if (!dados.sucess) {
+        logger.info("Tentou atualizar cupom inexistente com o nome: " + data.nome);
+        return { sucess: false, error: dados.error };
+      }
 
+      logger.info("Atualizou o Cupom de nome: " + data.nome + " com o valor: " + data.valor);
       await db.execute("UPDATE Cupom SET nome = ?, valor = ? WHERE nome = ?", [
         data.nome,
         data.valor,
@@ -79,8 +91,15 @@ export const CupomDB = {
   async deleteCupom(id: number): Promise<{ sucess: boolean; error?: string }> {
     try {
       const dados = await CupomDB.getById(id);
-      if (!dados.sucess) return { sucess: false, error: dados.error };
+      if (!dados.sucess) {
+        logger.info("Tentou deletar o cupom de ID: " + id);
+        return { sucess: false, error: dados.error };
+      }
 
+      const dados2 = await CupomDB.getById(id);
+      if (!dados2.sucess || !dados2.data) return { sucess: false };
+
+      logger.info("Deletou o Cupom: " + dados2.data[0].nome);
       await db.execute("DELETE FROM Cupom WHERE id = ?", [id]);
       return { sucess: true };
     } catch (err) {
@@ -93,6 +112,8 @@ export const CupomDB = {
   async deleteAll(): Promise<{ sucess: boolean; error?: string }> {
     try {
       await db.query("DELETE FROM Cupom");
+      logger.info("Deletou todos os cupons!");
+
       return { sucess: true };
     } catch (err) {
       logger.error("Cupom DeleteAllCupom: " + err);
